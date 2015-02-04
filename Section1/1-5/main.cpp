e00e87c44c92f64f55852c39f70e52e13220ccb4
hawashima <awaawashima@gmail.com>
2015-02-04 23:56:55 +0900

    1-5

---
+++ Section1/1-5/main.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int no;
+    cout << "®”“ü—Í‚µ‚Ä‚­‚¾‚³‚¢B" << endl;
+    cin >> no;
+    cout << no << "‚É12‰Á‚¦‚é‚Æ" << no + 12 << "‚Å‚·B" << endl;
+
+    return 0;
+}
