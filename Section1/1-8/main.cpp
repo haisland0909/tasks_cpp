d41736d342664e61acf478f030fe288f53ea8c8f
hawashima <awaawashima@gmail.com>
2015-02-05 00:05:02 +0900

    1-9

---
+++ Section1/1-8/main.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int no1, no2;
+    cout << "®”“ü—Í‚µ‚Ä‚­‚¾‚³‚¢B" << endl;
+    cin >> no1;
+    cout << "®”“ü—Í‚µ‚Ä‚­‚¾‚³‚¢B" << endl;
+    cin >> no2;
+    cout << no1 << "*" << no2 << " = " << no1 * no2 << endl;
+
+    return 0;
+}
---
+++ Section1/1-9/main.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int no1, no2, no3;
+    cout << "®”“ü—Í‚µ‚Ä‚­‚¾‚³‚¢B" << endl;
+    cin >> no1;
+    cout << "®”“ü—Í‚µ‚Ä‚­‚¾‚³‚¢B" << endl;
+    cin >> no2;
+    cout << "®”“ü—Í‚µ‚Ä‚­‚¾‚³‚¢B" << endl;
+    cin >> no3;
+
+    cout << no1 << " + " << no2 << " + " << no3 << " = " << no1 + no2 + no3 << endl;
+
+    return 0;
+}
