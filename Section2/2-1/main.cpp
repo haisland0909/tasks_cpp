9c73c85eca6168c424d19af374471f07c55baa21
hawashima <awaawashima@gmail.com>
2015-02-05 22:17:52 +0900

    2-1

--- Section2/2-1/main.cpp
+++ Section2/2-1/main.cpp
@@ -2,6 +2,12 @@
 using namespace std;
 
 int main() {
+    int no1, no2;
+    cout << "®”‚ð“ü—Í‚µ‚Ä‚­‚¾‚³‚¢" << endl;
+    cin >> no1;
+    cout << "®”‚ð“ü—Í‚µ‚Ä‚­‚¾‚³‚¢" << endl;
+    cin >> no2;
+    cout << no1 << "‚Í" << no2 << "‚Ì" << no1 * 100 / no2 << "%‚Å‚·B" << endl;
 
     return 0;
 }
